99e76ea66f0e993064c66b8eec3cbdfc295774a3
Marcio Casarin <casarin.marcio@gmail.com>
2019-02-21 13:07:47 -0300

    Add files via upload

---
+++ RotinaBackupCiclo.sh
@@ -0,0 +1,36 @@
+##################################
+#
+# Script de criação de diretório e execução de backup
+# Cria diretório, muda dono e altera permissões
+# Roda Dump diario de seg/sex
+# Roda dump semanal aos domingos
+# Data: 18nov18
+# mcasarin (casarin.marcio@gmail.com)
+#
+# cron para rodar no final do mês: 0 23 28-31 * * [ $(date -d +1day +%d) -eq 1 ] && /media/backup/CriarDirPermi.sh 
+# Atualizado para cron: 0 23 * * * /media/backup/RotinaBackupCiclo.sh
+#
+#################################
+
+diretorio="/media/backup/BKP-`date +\%m\%Y`"
+diasemana="`date +%u`"
+domingo="7"
+data="date +\%d-\%m-\%y"
+
+if [ ! -d $diretorio ]; then
+	#echo "$diretorio"
+	mkdir $diretorio
+	chown churchill:churchill $diretorio
+	chmod 777 $diretorio
+	echo "Diretorio criado!"
+else
+	echo "Não foi criado o diretorio"
+fi
+
+if [[ $diasemana == $domingo ]]; then
+	exec "mysqldump -h localhost -u root -pmysqlbanco nitcabs -R --opt --single-transaction > $diretorio/FULL-$data.sql" && exec "tar czf $diretorio/FULL-$data.tar.gz $diretorio/FULL-$data.sql" && exec "rm -rf $diretorio/FULL-$data.sql"
+else
+	exec "mysqldump -h localhost -u root -pmysqlbanco nitcabs --tables cartoes d`date +\%d\%m\%Y` empresas movvis visitantes operadores rede1 -R --opt --single-transaction > $diretorio/diario-$data.sql" && exec "tar czf $diretorio/diario-$data.tar.gz $diretorio/diario-$data.sql" && exec "rm -rf $diretrio/diario-$data.sql"
+fi 
+
+echo "FIM!"
